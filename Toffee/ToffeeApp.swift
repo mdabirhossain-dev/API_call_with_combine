4bb2a647893619da838b4b2009faa40a29c0838d
Md Abir Hossain <mdabirhossain.dev@gmail.com>
2024-07-16 21:58:41 +0600

    Project created.

---
+++ Toffee/ToffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToffeeApp.swift
+//  Toffee
+//
+//  Created by Md Abir Hossain on 16/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ToffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Toffee/View/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Toffee
+//
+//  Created by Md Abir Hossain on 16/7/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ListView()
+}
