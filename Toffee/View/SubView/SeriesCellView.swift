90a96b4b02e51df5d59cb683b865bb5a7a9a368a
Md Abir Hossain <mdabirhossain.dev@gmail.com>
2024-07-24 23:54:07 +0600

    Files relocated and separate file created for SeriesCellView

---
+++ Toffee/View/SubView/SeriesCellView.swift
@@ -0,0 +1,107 @@
+//
+//  SeriesCellView.swift
+//  Toffee
+//
+//  Created by Md Abir Hossain on 24/7/24.
+//
+
+import SwiftUI
+
+struct SeriesCellView: View {
+    
+    @EnvironmentObject var dataVM: DataViewModel
+    let data: DataResponse
+    let height: CGFloat
+    @State private var isLoadSuccess = true
+    @State private var noImage = UIImage(named: "noImage")
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            ZStack(alignment: .center) {
+//                Image(data.image)
+                WebImage(url: URL(string: data.image)) { image in
+                    image
+                        .resizable()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: height)
+                        .cornerRadius(2)
+                        .overlay (
+                            VStack(alignment: .center, spacing: 8) {
+                                Image(systemName: "play.rectangle.on.rectangle")
+                                    .font(.system(size: 18))
+                                
+                                Text("56 Episodes")
+                                    .font(.system(size: 10, weight: .semibold))
+                                    .multilineTextAlignment(.center)
+                            }
+                                .foregroundColor(Color.white)
+                                .frame(width: height / 1.6)
+                                .frame(maxHeight: .infinity)
+                                .background(Color.black.opacity(0.4))
+                            , alignment: .trailing
+                        )
+                } placeholder: {
+                    if isLoadSuccess {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: Color.red))
+                            .scaleEffect(1.5)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: height)
+                    } else {
+                        Image(uiImage: noImage!)
+                            .resizable()
+                            .frame(maxWidth: .infinity)
+                            .frame(height: height)
+                            .cornerRadius(2)
+                    }
+                }
+                .onFailure { _ in
+                    isLoadSuccess = false
+                }
+            }
+            
+            HStack(alignment: .top, spacing: 10) {
+                Text(data.name)
+                    .font(.system(size: 14, weight: .semibold))
+                    .lineLimit(2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Menu {
+                    ShareLink(item: URL(string: data.image) ?? URL(string: "https://viterbischool.usc.edu/wp-content/uploads/2023/10/404.jpg")!) {
+                        Label("Share to Others", systemImage: "square.and.arrow.up")
+                    }
+                    
+                    Button {
+                        print("Save button tapped...")
+                    } label: {
+                        Label("Save to Favourite", systemImage: "sdcard")
+                    }
+                    
+                    Button {
+                        if let index = self.dataVM.dataResponse.firstIndex(of: data) {
+                            self.dataVM.dataResponse.remove(at: index)
+                        }
+                        
+                        print("Delete button tapped...")
+                    } label: {
+                        Label("Delete from List", systemImage: "trash")
+                    }
+                } label: {
+                    Image(systemName: "ellipsis")
+                        .font(.system(size: 14, weight: .semibold))
+                        .rotationEffect(.degrees(90))
+                        .padding(8)
+                }
+            }
+            .foregroundColor(Color(red: 190/255, green: 214/255, blue: 242/255))
+            .padding(.top, 5)
+            
+            Text("Toffee • 2h")
+                .font(.system(size: 14))
+                .foregroundColor(Color(red: 121/255, green: 142/255, blue: 170/255))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer() // for alignment issue
+        }
+    }
+}
+
